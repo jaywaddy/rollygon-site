1d2e404a64e707322dc0aa582fc363127384e133
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2025-05-21 21:31:28 -0700

    Build Header and Button components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
@@ -7,7 +8,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
+	vite: {
     plugins: [tailwindcss()]
   },
 
---
+++ src/scripts/slugify.ts
@@ -0,0 +1,8 @@
+export function slugify (input: string) {
+    return input
+        .replace(/^\s+|\s+$/g, '') // trim leading/trailing white space
+        .toLowerCase() // convert string to lowercase
+        .replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+        .replace(/\s+/g, '-') // replace spaces with hyphens
+        .replace(/-+/g, '-') // remove consecutive hyphens
+}
