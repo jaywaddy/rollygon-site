9ef313410a43d545cbd12d0f66e5775968cd27fc
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2025-06-21 14:03:08 -0700

    Edit banners and marquees

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,2 +1,5 @@
 export const SITE_HEADING = "I make custom tools in Blender & roller coasters out of polygons";
-export const SITE_SUB_HEADING = "As I learned Blender, I created a few add-ons to build faster. These are those add-ons."
+export const SITE_SUB_HEADING = "I made a few tools to model faster. These are all the models and the tools!"
+export const NEW_MODELS_COUNT = 1;
+export const NEW_TOOLS_COUNT = 3;
+export const NEW_ARTICLES_COUNT = 3;
