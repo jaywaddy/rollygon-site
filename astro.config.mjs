b48a1a965080d32467614fc15db61c5ef063039c
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2025-10-11 22:33:37 -0700

    Add pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  output: "static",
+  adapter: netlify(),
 });
