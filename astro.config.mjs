27f70163442785826b3412b9106b3a071e79cf60
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2026-06-16 08:58:05 -0700

    Set up Sanity structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +13,17 @@ export default defineConfig({
   },
   output: "static",
   adapter: netlify(),
+  integrations: [
+    sanity({
+      projectId: "liw3qu8s",
+      dataset: "production",
+      // Set useCdn to false if you"re building statically.
+      useCdn: false,
+      // Optional: log server-side Sanity client requests.
+      // Modes: "dev" | "build" | "always"
+      logClientRequests: "dev",
+      studioBasePath: "/admin",
+    }),
+    react()
+  ],
 });
