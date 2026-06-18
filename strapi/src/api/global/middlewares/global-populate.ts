f2e468940aa6fc9042afca71b358ef281c4bed97
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2026-06-18 13:30:39 -0700

    Astro init

---
+++ strapi/src/api/global/middlewares/global-populate.ts
@@ -0,0 +1,48 @@
+/**
+ * `global-populate` middleware
+ */
+
+import type { Core } from '@strapi/strapi';
+
+const link = {
+  populate: {
+    icon: {
+      fields: ["alternativeText", "url"]
+    }
+  }
+};
+
+const populate = { 
+  banner: { 
+    populate: { link } 
+  }, 
+
+  header: { 
+    populate: { 
+      logo: link,
+      navLinks: link,
+      cta: link, 
+    } 
+  }, 
+
+  footer: { 
+    populate: { 
+      logo: link,
+      group: {
+        populate:{
+          footerLink: link
+        }
+      }
+    } 
+  } 
+};
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    ctx.query.populate = populate;
+    strapi.log.info('In global-populate middleware.');
+
+    await next();
+  };
+};
--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,3 +1,87 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface LayoutBanner extends Struct.ComponentSchema {
+  collectionName: 'components_layout_banners';
+  info: {
+    displayName: 'Banner';
+    icon: 'code';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    isVisible: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.Component<'shared.link', false>;
+  };
+}
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+    icon: 'code';
+  };
+  attributes: {
+    copyrightText: Schema.Attribute.Text;
+    group: Schema.Attribute.Component<'layout.footer-group', true>;
+    logo: Schema.Attribute.Component<'shared.link', false>;
+  };
+}
+
+export interface LayoutFooterGroup extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footer_groups';
+  info: {
+    displayName: 'FooterGroup';
+    icon: 'code';
+  };
+  attributes: {
+    footerLink: Schema.Attribute.Component<'shared.link', true>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+    icon: 'code';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'shared.link', false>;
+    logo: Schema.Attribute.Component<'shared.link', false>;
+    navLinks: Schema.Attribute.Component<'shared.link', true>;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+    icon: 'link';
+  };
+  attributes: {
+    href: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'/'>;
+    icon: Schema.Attribute.Media<'images'>;
+    iconPosition: Schema.Attribute.Enumeration<['LEFT', 'RIGHT']> &
+      Schema.Attribute.DefaultTo<'LEFT'>;
+    isButtonLink: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+    showIcon: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    showLabel: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<true>;
+    style: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']> &
+      Schema.Attribute.DefaultTo<'PRIMARY'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.banner': LayoutBanner;
+      'layout.footer': LayoutFooter;
+      'layout.footer-group': LayoutFooterGroup;
+      'layout.header': LayoutHeader;
+      'shared.link': SharedLink;
+    }
+  }
+}
