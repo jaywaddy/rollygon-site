fd865fd28aea86190388b8a36f9a305936a949c5
Jay Waddy <40751612+jaywaddy@users.noreply.github.com>
2026-06-18 19:13:35 -0700

    Set up environment

--- astro/src/types/index.ts
+++ astro/src/types/index.ts
@@ -1,44 +1,43 @@
 // Strapi native
-
 export type TImage = {
-    alternativeText: string;
-    documentId: string;
-    id: number;
-    url: string;
-}
+	alternativeText: string;
+	documentId: string;
+	id: number;
+	url: string;
+};
 
 // Shared components
 export type TLink = {
-    href: string;
-    icon?: TImage;
-    iconPosition?: "LEFT" | "RIGHT";
-    isButtonLink: boolean;
-    isExternal: boolean;
-    label: string;
-    showIcon: boolean;
-    showLabel: boolean;
-    style?: "PRIMARY" | "SECONDARY";
-}
+	href: string;
+	icon?: TImage;
+	iconPosition?: "LEFT" | "RIGHT";
+	isButtonLink: boolean;
+	isExternal: boolean;
+	label: string;
+	showIcon: boolean;
+	showLabel: boolean;
+	style?: "PRIMARY" | "SECONDARY";
+};
 
 // Layout components
 export type TBanner = {
-    description: string;
-    isVisible: boolean;
-    link: TLink;
-}
+	description: string;
+	isVisible: boolean;
+	link: TLink;
+};
 
 export type TFooter = {
-    groups: TFooterGroup[];
-    logo: TLink;
-}
+	groups: TFooterGroup[];
+	logo: TLink;
+};
 
 export type TFooterGroup = {
-    footerLinks: TLink[];
-    title: string;
-}
+	footerLinks: TLink[];
+	title: string;
+};
 
 export type THeader = {
-    logo: TLink;
-    navLinks: TLink[];
-    cta: TLink;
-}
+	logo: TLink;
+	navLinks: TLink[];
+	cta: TLink;
+};
